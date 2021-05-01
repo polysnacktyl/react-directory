b159941e80d11e8e7478345b193b50623be9b9df
Amanda Hamilton <77996281+polysnacktyl@users.noreply.github.com>
2021-05-01 12:04:24 -0400

    pput default export formatting for a proper API calling experience

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,18 +1,7 @@
 import axios from 'axios';
 
-randos();
-async function randos() {
-
-axios.get("https://randomuser.me/api/?results=5&inc=name,location,email,id,picture")
-
-.then(response => {
-    console.log(response.data.results);
-})
-
-.catch(error => {
-    console.log(error);
-})
-}
-
-
-//this.setState({users: response.data.resuts})
+export default {
+    summonRandos: function(employee) {
+      return axios.get("https://randomuser.me/api/?results=5");
+    }
+  };
