c3a55657f5b0bcb9462aea74c70a31d2d7c90c80
Amanda Hamilton <77996281+polysnacktyl@users.noreply.github.com>
2021-05-03 17:17:08 -0400

    set api result export to a variable to get rid of a console warning

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,20 @@
 import axios from 'axios';
 
-export default {
-    summonRandos: function(employee) {
-      return axios.get("https://randomuser.me/api/?results=5");
-    }
-  };
+// export default {
+//     summonRandos: function() {
+//       return axios.get("https://randomuser.me/api/?results=5");
+//     }
+//   };
+// lines 3-7 achieve desired results, but lines 10-19 do the same 
+// job without an anonymous export warning. cool good talk. 
+
+function summonRandos() {
+  return axios.get("https://randomuser.me/api/?results=50");
+}
+
+const API = {
+  summonRandos
+}
+
+export default API;
+
