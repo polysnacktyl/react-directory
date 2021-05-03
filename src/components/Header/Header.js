d7d7a00025d46fe5be0fc76d5e6689be5a57fefd
Amanda Hamilton <77996281+polysnacktyl@users.noreply.github.com>
2021-05-03 17:34:30 -0400

    jumbotron and its styling

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+                <h1 className="display-4">Employee Directory</h1>
+            </div>
+
+    )
+};
+
+export default Header;
+
